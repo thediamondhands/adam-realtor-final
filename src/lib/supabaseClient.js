49f0683843898fa6a16025ee575daa6130e625be
thediamondhands <79345119+thediamondhands@users.noreply.github.com>
2026-04-23 16:35:28 -0400

    Create supabaseClient.js

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-client'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
